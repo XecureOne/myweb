f7ba10b6ccb06bb2dfe9ac91d7134a7a532ab48c
BlackDeath <vsudhareshan@gmail.com>
2025-08-13 22:12:24 +0530

    second commit

--- frontend/src/Admin/api/api.js
+++ frontend/src/Admin/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URI,
+  baseURL: ,
 });
 console.log("BASE URL:", import.meta.env.VITE_SERVER_URI);
 
