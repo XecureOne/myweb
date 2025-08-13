b34889c9236897ec57458a6f6b6e7d8635a45c87
BlackDeath <vsudhareshan@gmail.com>
2025-08-13 23:02:29 +0530

    Fixing admin

--- frontend/src/Admin/api/api.js
+++ frontend/src/Admin/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://myweb-cwar.onrender.com",
+  baseURL: "https://myweb-cwar.onrender.com/api",
 });
 
 export default API;
